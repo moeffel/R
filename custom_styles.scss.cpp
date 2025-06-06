4c96190828590e324ab21cd1b48bb4977843d2b9
Markus <markus.oeffel@gmail.com>
2025-06-06 12:35:02 +0200

    Add files via upload

---
+++ custom_styles.scss.cpp
@@ -0,0 +1,6 @@
+# /* www/custom_styles.scss */
+# .card-header { font-weight: 600; background-color: rgba(0,0,0,0.03) !important; }
+# .value-box .value-box-title { font-size: 0.8rem !important; text-transform: uppercase; color: #555; }
+# .value-box .value-box-value { font-size: 1.5rem !important; font-weight: 700; }
+# table.dataTable th { font-size: 0.85em !important; }
+# table.dataTable td { font-size: 0.85em !important; }
